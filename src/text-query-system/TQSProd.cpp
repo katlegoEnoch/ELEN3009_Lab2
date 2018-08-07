de9a4d8f10929dbd41b2c2b0b8b8db00af444501
Katlego Gagoopane <0705657y@students.wits.ac.za>
2018-08-07 17:43:02 +0200

    Exercise 5.5 - Text Query System

--- src/text-query-system/TQSProd.cpp
+++ src/text-query-system/TQSProd.cpp
@@ -5,15 +5,55 @@
 
 #include <string>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
 int main()
 {
-    //it must be in the same directory as the executable
-	auto filereader = FileReader{"alice.txt"};
-	auto paragraph = Paragraph{};
-	filereader.readFileInto(paragraph);
-
+    //declare program variables
+    string fileName;
+    string searchWord;
+    
+    //variables to store process outputs
+    vector<int> lineNumbers;
+    bool containsWord;
+    
+    //prompt user for input
+    cout << "Please enter file name: ";
+    cin  >>  fileName;
+    
+    //if file not found, exception will be thrown and will need to be caught
+    auto filereader = FileReader{fileName};
+    auto paragraph = Paragraph{};
+    filereader.readFileInto(paragraph);
+    
+    do{
+        //prompt user for further input
+        cout << "\nPlease enter a word to search for or '.' to quit: ";
+        cin  >> searchWord;
+        //create word from string, query paragraph for word
+        //Word will throw exception after . 'punct' is deleted
+        try{
+        std::tie (containsWord,lineNumbers) = paragraph.contains(Word{searchWord});
+        }catch(WordContainsNoLetters){
+            //do nothing
+        }
+        //process search results
+        //using search results we can display outcome
+        if(containsWord){
+            //display first line
+            cout << "\nWord found: line " << lineNumbers.at(0);
+            //print other lines containing word
+            for(string::size_type line = 1; line < lineNumbers.size();line++){
+                cout << "\n" <<setw(17) << "Line " << lineNumbers.at(line);
+            }//end for loop
+            cout << endl;
+        }//end if
+        else
+            cout << "\nWord not found" << endl;
+    }while(searchWord != ".");
+    
+    
     return 0;
 }
